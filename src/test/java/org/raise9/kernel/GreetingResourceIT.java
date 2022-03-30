13f5adf9e5648aa34de2baa540611e5728e803a2
quarkusio <no-reply@quarkus.io>
2022-03-30 13:02:22 +0000

    Initial commit

---
+++ src/test/java/org/raise9/kernel/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.raise9.kernel;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
